160d93315b2834884f3ae69ba0da5b59dd0a71e6
Gustavo Maia <ms.gustavo@outlook.com>
2024-07-23 13:41:38 -0300

    Creating user model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: "CLIENT" | "THERAPIST";
+  speciality?: string;
+  location?: {
+    type: "Point";
+    coordinates: [number, number];
+  };
+}
+
+const userSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  speciality: { type: String },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: false,
+    },
+    coordinates: {
+      type: [Number],
+      required: false,
+    },
+  },
+});
+
+userSchema.index({ location: "2dsphere" });
+
+export default mongoose.model<IUser>("User", userSchema);
